ef13e36894bb8a8aafe57010558fbc5f1d1c5889
Francisco Albiar Jiménez <francisco.albiar@hotmail.com>
2025-03-22 21:54:56 +0100

    Terminada la lógica de las animaciones del Mudler básico
    Actualizados también el prefab del Mudler y el de Rufus

--- Assets/Scripts/BasicMudlerMovement.cs
+++ Assets/Scripts/BasicMudlerMovement.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
+using System.Collections;
 
 public class BasicMudlerMovement : MonoBehaviour
 {
     public float speed = 3f; //velocidad de movimiento del Mudler
     private int direction = 1; //dirección inicial (1 = adelante, -1 = atrás)
     public int health = 3; //vida del enemigo
+    public float shrinkSpeed = 0.6f; //velocidad a la que el enemigo se encoge
+    public float hurtDuration = 0.5f; //duración de la animación de daño
+    public float dyingDuration = 10f; //duración de la animación de muerte
+    private bool isDying = false; //para saber si el enemigo se está muriendo (y evitar que se mueva mientras se muere y que le podamos seguir golpeando)
     private Rigidbody rb;
 
     public GameObject coinPrefab; //prefab de la moneda
     public GameObject littleWaterBottlePrefab; //prefab de la botella de agua pequeña
     public GameObject keyPrefab; //prefab de la llave
+
+    public Animator animator; //Animator para cambiar entre las distintas animaciones
 
     public static int enemiesDefeated = 0; //contador de Mudlers derrotados por Milton
     public int[] enemiesWithKey = {1,3,6,7}; //números en los que los enemigos tienen que soltar la llave del nivel (en la primera sala el enemigo que hay, en la segunda los dos, en la 4 los 3 y en la última el Mudler especial)
@@ -22,12 +29,18 @@ public class BasicMudlerMovement : MonoBehaviour
 
     private void Update()
     {
-        //mueve al enemigo en el eje Z
-        transform.position += new Vector3(0, 0, direction * speed * Time.deltaTime);
+        //si el enemigo NO se está muriendo, se mueve
+        if (!isDying)
+        {
+            //mueve al enemigo en el eje Z
+            transform.position += new Vector3(0, 0, direction * speed * Time.deltaTime);
+        }
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
+
         //si choca con una pared, cambia de dirección
         if (other.CompareTag("Wall"))
         {
@@ -41,8 +54,8 @@ public class BasicMudlerMovement : MonoBehaviour
             Destroy(other.gameObject); //destruye la bala al ser recibida
         }
 
-        //si choca con Milton, le hace daño (método en el Script de Milton)
-        if (other.CompareTag("Player"))
+        //si choca con Milton y no está muriéndose, le hace daño (método en el Script de Milton)
+        if (other.CompareTag("Player")  && !isDying)
         {
             other.GetComponent<MiltonLogic>().TakeDamage(transform.position);
         }
@@ -50,13 +63,75 @@ public class BasicMudlerMovement : MonoBehaviour
 
     private void TakeDamage()
     {
-        health--;
-
-        if (health <= 0)
+        //si el enemigo NO se está muriendo, recibe daño y demás
+        if (!isDying)
         {
-            DropItems();
-            Destroy(gameObject); //elimina al enemigo si su vida llega a 0
+            health--;
+
+            animator.SetBool("isHurt", true);
+
+            if (health <= 0)
+            {
+                isDying = true;
+
+                //destruir el Collider antes de destruir el objeto
+                Destroy(GetComponent<Collider>());
+
+                //activar la animación de muerte
+                animator.SetBool("isDying", true);
+
+                //detener movimiento y demás comportamientos
+                rb.linearVelocity = Vector3.zero;
+
+                //iniciar el encogimiento hacia abajo
+                StartCoroutine(ShrinkTowardsGround());
+
+                //esperar la duración de la animación antes de destruir el objeto
+                StartCoroutine(WaitForDeathAnimation());
+                DropItems();
+            }
+            else
+            {
+                StartCoroutine(ResetHurtAnimation());
+            }
         }
+    }
+
+    IEnumerator ResetHurtAnimation()
+    {
+        // Espera el tiempo de la animación de daño
+        yield return new WaitForSeconds(hurtDuration); // Ajusta este tiempo según la duración de la animación de daño
+
+        // Después de la animación de daño, desactivar el parámetro 'isHurt'
+        animator.SetBool("isHurt", false);
+    }
+
+    IEnumerator ShrinkTowardsGround()
+    {
+        Vector3 initialScale = transform.localScale;
+
+        //desactivar la gravedad mientras se encoge para que no flote hacia arriba
+        rb.useGravity = false;
+
+        //reduce la escala en Y hasta llegar a 0 (o a un valor pequeño para simular el charco de barro)
+        while (transform.localScale.y > 0.1f)
+        {
+            transform.localScale = new Vector3(initialScale.x, transform.localScale.y - shrinkSpeed * Time.deltaTime, initialScale.z);
+            yield return null;
+        }
+
+        transform.localScale = new Vector3(initialScale.x, 0, initialScale.z); //para que se quede completamente aplastado
+
+        rb.useGravity = true; //volver a activar la gravedad
+    }
+
+    IEnumerator WaitForDeathAnimation()
+    {
+        //espera el tiempo de duración de la animación de muerte
+        yield return new WaitForSeconds(dyingDuration); // Ajusta este tiempo según la duración de la animación de muerte
+
+        //después de la animación, destruir al enemigo
+        Destroy(gameObject);
     }
 
     private void DropItems()
