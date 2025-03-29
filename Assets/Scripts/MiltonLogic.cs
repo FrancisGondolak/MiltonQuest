7915ec34242cae0f3352403eee7b62234ceff6d3
Francisco Albiar Jiménez <francisco.albiar@hotmail.com>
2025-03-29 18:27:45 +0100

    Dada la forma y situación de la 3 sala

--- Assets/Scripts/MiltonLogic.cs
+++ Assets/Scripts/MiltonLogic.cs
@@ -184,8 +184,8 @@ public class MiltonLogic : MonoBehaviour
 
         if (other.gameObject.CompareTag("Door"))
         {
-            if (inventoryManager.hasKey)
-            {
+            //if (inventoryManager.hasKey)
+            //{
                 Door door = other.gameObject.GetComponent<Door>(); //obtiene el script de la puerta
                 if (door != null)
                 {
@@ -193,11 +193,11 @@ public class MiltonLogic : MonoBehaviour
                     inventoryManager.hasKey = false; //pierde la llave al cambiar de sala
                     inventoryManager.UpdateInventoryUI();
                 }
-            }
-            else
-            {
-                Debug.Log("MILTON NO TIENE LA LLAVE");
-            }
+            //}
+            //else
+            //{
+                //Debug.Log("MILTON NO TIENE LA LLAVE");
+            //}
         }
     }
 
